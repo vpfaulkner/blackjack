ab19a20d8b6782eb7c7cfa9195e6aff5ea91bf8a
vpfaulkner@gmail.com <vpfaulkner@gmail.com>
2014-09-18 14:07:13 -0400

    Established classes and major instance variables

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,10 +1,17 @@
 class Blackjack
   def run
-
-
+    game = Game.new
+    while game.play_again?
+      game.get_name
+      game.get_bet
+      game.play
+      game.adjust_winnings
+      game.play_again?
+    end
     fail "You need to add some functionality here before you can use this."
   end
 end
+
 
 class Deck
 
@@ -14,6 +21,10 @@ end
 
 class Player
 
+  def deal
+
+  end
+
   hand = []
 
 end
@@ -22,16 +33,43 @@ class Dealer
 
   hand = []
 
+  def deal
+
+  end
+
 end
 
-class Gameplay
+class Game
 
-  def get_count
+  def initialize
+    @player_name = nil
+    @winnigs = nil
+    @score = nil
 
-  end
-
-  def get_count
-
-  end
+    @wager = nil
+    @result = nil
     
+  end
+
+  def get_name
+
+  end
+
+  def get_bet
+
+  end
+
+  def play
+
+  end
+
+
+  def adjust_winnings
+
+  end
+
+  def play_again?
+
+  end
+
 end
