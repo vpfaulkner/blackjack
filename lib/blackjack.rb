2b128d417d3ef814093b7f7a97f9c4d63d4535c7
vpfaulkner@gmail.com <vpfaulkner@gmail.com>
2014-09-18 13:31:57 -0400

    First commit of blank Wren project

---
+++ lib/blackjack.rb
@@ -0,0 +1,37 @@
+class Blackjack
+  def run
+
+
+    fail "You need to add some functionality here before you can use this."
+  end
+end
+
+class Deck
+
+  deck = {}
+
+end
+
+class Player
+
+  hand = []
+
+end
+
+class Dealer
+
+  hand = []
+
+end
+
+class Gameplay
+
+  def get_count
+
+  end
+
+  def get_count
+
+  end
+    
+end
